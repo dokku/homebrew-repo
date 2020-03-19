0594fab79219e67a1f4da109e752b768b9c33284
Milan Vit <milanvit@milanvit.net>
2020-03-19 11:22:14 +0900

    🎨 Fix terrible indentation
    I'm terribly sorry

--- Formula/dokku.rb
+++ Formula/dokku.rb
@@ -1,26 +1,26 @@
 class Dokku < Formula
-    desc "Command-line client for the Dokku PaaS"
-    homepage "http://dokku.viewdocs.io"
-    url "https://github.com/dokku/dokku/archive/v0.19.13.tar.gz"
-    sha256 "d5f651d43c2fa645f3be874b56679ce966ccd87f5df68bc27d06920ec812af7c"
+  desc "Command-line client for the Dokku PaaS"
+  homepage "http://dokku.viewdocs.io"
+  url "https://github.com/dokku/dokku/archive/v0.19.13.tar.gz"
+  sha256 "d5f651d43c2fa645f3be874b56679ce966ccd87f5df68bc27d06920ec812af7c"
 
-    def install
-      bin.install "contrib/dokku_client.sh" => "dokku"
-    end
-
-    def caveats; <<~EOS
-      Run `dokku` from a repository with a git remote named `dokku` pointed
-      at your Dokku host in order to use the script as normal, e.g.:
-        git remote add dokku dokku@<dokku-host>:<app-name>
-      or configure the `DOKKU_HOST`, `DOKKU_PORT` and `DOKKU_GIT_REMOTE`
-      environment variables, e.g.:
-        export DOKKU_HOST=dokku.me
-        export DOKKU_PORT=22
-        export DOKKU_GIT_REMOTE=dokku
-      EOS
-    end
-
-    test do
-      system bin/"dokku"
-    end
+  def install
+    bin.install "contrib/dokku_client.sh" => "dokku"
   end
+
+  def caveats; <<~EOS
+    Run `dokku` from a repository with a git remote named `dokku` pointed
+    at your Dokku host in order to use the script as normal, e.g.:
+      git remote add dokku dokku@<dokku-host>:<app-name>
+    or configure the `DOKKU_HOST`, `DOKKU_PORT` and `DOKKU_GIT_REMOTE`
+    environment variables, e.g.:
+      export DOKKU_HOST=dokku.me
+      export DOKKU_PORT=22
+      export DOKKU_GIT_REMOTE=dokku
+    EOS
+  end
+
+  test do
+    system bin/"dokku"
+  end
+end
