dbfbc61af1b52217857adb57d38d618849c0df18
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2021-04-16 14:44:20 -0400

    dokku 0.24.5

--- Formula/dokku.rb
+++ Formula/dokku.rb
@@ -1,8 +1,8 @@
 class Dokku < Formula
   desc "Command-line client for the Dokku PaaS"
   homepage "https://dokku.com"
-  url "https://github.com/dokku/dokku/archive/v0.24.3.tar.gz"
-  sha256 "482b76d14d0e68b9030624c4e9d2db6db5c6177852d44dc2f8d5eba7cac4f982"
+  url "https://github.com/dokku/dokku/archive/v0.24.5.tar.gz"
+  sha256 "b1ccb663b5eac6fcaff566ef53428d416a394679f511492c1fda34927a429777"
 
   def install
     bin.install "contrib/dokku_client.sh" => "dokku"
