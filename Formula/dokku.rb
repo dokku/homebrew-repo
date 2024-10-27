b990b9ad157aacd9ce5d63946a312062f6f0670a
Ahmad Younes <ahmayou@gmail.com>
2024-10-27 15:00:22 +0800

    dokku 0.35.8

--- Formula/dokku.rb
+++ Formula/dokku.rb
@@ -1,8 +1,8 @@
 class Dokku < Formula
   desc "Command-line client for the Dokku PaaS"
   homepage "https://dokku.com"
-  url "https://github.com/dokku/dokku/archive/v0.34.7.tar.gz"
-  sha256 "7d6ab68aae0df069e37cc94206c4268874665509b87aa30a7b3a60045cb5c6a2"
+  url "https://github.com/dokku/dokku/archive/v0.35.8.tar.gz"
+  sha256 "ec97d6ec273add86743310b2aa33c9c530975a4240aab90e25a408d0cd69ec97"
 
   def install
     bin.install "contrib/dokku_client.sh" => "dokku"
